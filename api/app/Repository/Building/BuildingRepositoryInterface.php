db7bf90a55d320edfb71529d0dbd8b16f3cec62a
Somov Alexey Alexandrovich <a.somov@robo.finance>
2023-04-20 23:55:18 +0700

    Add Building, container DI

---
+++ api/app/Repository/Building/BuildingRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository\Building;
+
+use App\Model\Building\Building;
+use App\Model\Building\BuildingCollection;
+use App\Repository\RepositoryInterface;
+
+interface BuildingRepositoryInterface extends RepositoryInterface
+{
+    public function save(Building $building);
+
+    public function create(Building $building): void;
+
+    public function getList(): BuildingCollection;
+
+    public function update(Building $building): void;
+}
